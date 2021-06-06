faf2a85aae2a387af1310a919a0dbbcdf1e6b4cb
Olazs <warplanes96@hotmail.com>
2021-06-06 22:50:45 +0200

    Hasznalhato mar nagyjabol

---
+++ main.py
@@ -0,0 +1,203 @@
+import pygame
+import random
+
+s_width=800
+s_height=600
+p_width=300
+p_height=200
+block_size=30
+
+topleft_x=(s_width-p_width) //2
+topleft_y=s_height-p_height
+
+#alakzatok
+S = [['.....',
+      '.....',
+      '..00.',
+      '.00..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['..0..',
+      '..0..',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '0000.',
+      '.....',
+      '.....',
+      '.....']]
+
+O = [['.....',
+      '.....',
+      '.00..',
+      '.00..',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+#alakzat definicio
+
+shapes = [S, Z, I, O, J, L, T]
+shape_colors = [(0,255,0),(255,0,0),(0,255,255),(255,255,0),(255, 200,50),(255,200,250),(120,120,120)]
+
+class darab(object):
+        rows=20
+        coloumns=10
+
+        def __init__(self, coloumn, row, shape):
+            self.x=coloumn
+            self.y=row
+            self.shape=shape
+            self.color=shape.colors[shapes.index(shape)]
+            self.rotation=0
+def create_grind(locked_positions={}):
+    grid=[[(0,0,0)] for x in range(10)] in range(20)
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j,i) in locked_positions:
+                c = locked_positions[(j,i)]
+                grid[i][j] = c
+    return grid
+
+#meghivas, atnevezesnel elromlik az egesz.
+def convert_shape_format(shape):
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+
+    return positions
+
+#helyvizsgalat
+def valid_space(shape, grid):
+    accepted_positions = [[(j, i) for j in range(10) if grid[i][j] == (0, 0, 0)] for i in range(20)]
+    accepted_positions = [j for sub in accepted_positions for j in sub]
+    formatted = convert_shape_format(shape)
+
+    for pos in formatted:
+        if pos not in accepted_positions:
+            if pos[1] > -1:
+                return False
+
+    return True
+
+
+def check_lost(positions):
+    for pos in positions:
+        x, y = pos
+        if y < 1:
+            return True
+    return False
+
+#uj forma letrehozasa
+def get_shape():
+    global shapes, shape_colors
+
+    return Piece(5, 0, random.choice(shapes))
+
+
+def draw_text_middle(text, size, color, surface):
+    font = pygame.font.SysFont('comicsans', size, bold=True)
+    label = font.render(text, 1, color)
+
+    surface.blit(label, (
+    top_left_x + play_width / 2 - (label.get_width() / 2), top_left_y + play_height / 2 - label.get_height() / 2))
+#Ezzel a szoveget iratjuk ki kozepen, kesobb van szerepe a jatek inditasanal es az esetleges kijatszas eseten.
+
+def draw_grid(surface, row, col):
+    sx = top_left_x
+    sy = top_left_y
+    for i in range(row):
+        pygame.draw.line(surface, (128, 128, 128), (sx, sy + i * 30),
+                         (sx + play_width, sy + i * 30))  # vizszintes
+        for j in range(col):
+            pygame.draw.line(surface, (128, 128, 128), (sx + j * 30, sy),
+                             (sx + j * 30, sy + play_height))  # fuggoleges
+
+
+main_menu()
