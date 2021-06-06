66f9e2ab3f6a262aae12d7fc7c567348cc3743dd
Olazs <warplanes96@hotmail.com>
2021-06-07 00:11:21 +0200

    keszen van a program, bemutatofilm van hatra csak.

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ def get_shape():
 
 
 def draw_text_middle(text, size, color, surface):
-    font = pygame.font.SysFont('comicsans', size, bold=True)
+    font = pygame.font.SysFont('msoutlook', size, bold=True)
     label = font.render(text, 1, color)
 
     surface.blit(label, (
@@ -221,7 +221,7 @@ def clear_rows(grid, locked):
                 locked[newKey] = locked.pop(key)
 
 def draw_next_shape(shape, surface):
-    font = pygame.font.SysFont('comicsans', 30)
+    font = pygame.font.SysFont('msoutlook', 30)
     label = font.render('Kovetkezo elem', 1, (255,255,255))
 
     sx = topleft_x + p_width + 50
@@ -237,8 +237,8 @@ def draw_next_shape(shape, surface):
     surface.blit(label, (sx + 10, sy- 30))
 def draw_window(surface):
     surface.fill((0,0,0))
-    # Tetris Title
-    font = pygame.font.SysFont('comicsans', 60)
+    # cim meg ezek
+    font = pygame.font('msoutlook', 60)
     label = font.render('TETRIS', 1, (255,255,255))
 
     surface.blit(label, (topleft_x + p_width / 2 - (label.get_width() / 2), 30))
@@ -247,10 +247,117 @@ def draw_window(surface):
         for j in range(len(grid[i])):
             pygame.draw.rect(surface, grid[i][j], (topleft_x + j* 30, topleft_y + i * 30, 30, 30), 0)
 
-    # draw grid and border
+    # itt kellene neki megoldania minden kiiratast, ez egy masolt reszlet, MIVEL NEKEM NEM AKAR MUKODNI
+    #Hihetetlen mennyi szenvedes van ezzel a pygame-vel, hivatkozgatni kell ra es meg az ember ne legyen ideges
+
     draw_grid(surface, 20, 10)
     pygame.draw.rect(surface, (255, 0, 0), (topleft_x, topleft_y, p_width, p_height), 5)
     # pygame.display.update()
+def main():
+    global grid
 
+    locked_positions = {}
+    grid = create_grid(locked_positions)
+    change_piece = False
+    run = True
+    current_piece = get_shape()
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    fall_time = 0
+    level_time = 0
+    fall_speed = 0.27
+    score = 0
+    while run:
+
+        grid = create_grid(locked_positions)
+        fall_time += clock.get_rawtime()
+        level_time += clock.get_rawtime()
+        clock.tick()
+
+        if level_time/1000 > 4:
+            level_time = 0
+            if fall_speed > 0.15:
+                fall_speed -= 0.005
+
+    #itt esnek a cuccok. Talaltam egy hasonlo kodot a neten (persze bugos volt) tehat vagy 3 ora stack overflow utan egy kilencedikes jott be azzal az otlettel,
+    #hogy heggesszem ossze oket. Szerintem Nagy Marcellnak jar az otos ezert a tippert.
+    if fall_time / 1000 >= fall_speed:
+        fall_time = 0
+        current_piece.y += 1
+        if not (valid_space(current_piece, grid)) and current_piece.y > 0:
+            current_piece.y -= 1
+            change_piece = True
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.display.quit()
+            quit()
+
+        if event.type == pygame.KEYDOWN: #alap erzekeles
+            if event.key == pygame.K_LEFT: #balramozgas
+                current_piece.x -= 1
+                if not valid_space(current_piece, grid):
+                    current_piece.x += 1
+
+            elif event.key == pygame.K_RIGHT: #jobbra
+                current_piece.x += 1
+                if not valid_space(current_piece, grid):
+                    current_piece.x -= 1
+            elif event.key == pygame.K_UP: #forgas
+                current_piece.rotation = current_piece.rotation + 1 % len(current_piece.shape)
+                if not valid_space(current_piece, grid):
+                    current_piece.rotation = current_piece.rotation - 1 % len(current_piece.shape)
+            if event.key == pygame.K_DOWN: #lefele mozgas alias speedup
+                current_piece.y += 1
+                if not valid_space(current_piece, grid):
+                    current_piece.y -= 1
+    shape_pos = convert_shape_format(current_piece)
+
+    #kirajzolas
+    for i in range(len(shape_pos)):
+        x, y = shape_pos[i]
+        if y > -1:
+            grid[y][x] = current_piece.color
+    #Foldeteres:
+    if change_piece:
+        for pos in shape_pos:
+            p = (pos[0], pos[1])
+            locked_positions[p] = current_piece.color
+        current_piece = next_piece
+        next_piece = get_shape()
+        change_piece = False
+
+        if clear_rows(grid, locked_positions): #Ha foldeter kigyulik a sor, akkor pontot kap
+            score += 10
+
+    draw_window(win)
+    draw_next_shape(next_piece, win)
+    pygame.display.update()
+
+    if check_lost(locked_positions):#hamar nem lenne kitorolheto sor
+        run = False
+
+    draw_text_middle("Vesztettel", 40, (255,255,255), win)
+    pygame.display.update()
+    pygame.time.delay(2000)
+
+def main_menu():
+    run = True #jatek inditasa
+    while run:
+        win.fill((0,0,0))
+        draw_text_middle('nyomj egy gombot az inditashoz.', 60, (255, 255, 255), win)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                main()
+    pygame.quit()
+
+
+win = pygame.display.set_mode((s_width, s_height))
+pygame.display.set_caption('Tetris .')
 
 main_menu()
