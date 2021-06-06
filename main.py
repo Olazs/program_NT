35760a9c778624b30d449be8a4024008fecdcf91
Olazs <warplanes96@hotmail.com>
2021-06-06 23:07:23 +0200

    sortores hozzaadva, meg nincs menu, letrehozasra var

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ def check_lost(positions):
 def get_shape():
     global shapes, shape_colors
 
-    return Piece(5, 0, random.choice(shapes))
+    return darab(5, 0, random.choice(shapes))
 
 
 def draw_text_middle(text, size, color, surface):
@@ -186,18 +186,38 @@ def draw_text_middle(text, size, color, surface):
     label = font.render(text, 1, color)
 
     surface.blit(label, (
-    top_left_x + play_width / 2 - (label.get_width() / 2), top_left_y + play_height / 2 - label.get_height() / 2))
+    topleft_x + p_width / 2 - (label.get_width() / 2), topleft_y + p_height / 2 - label.get_height() / 2))
 #Ezzel a szoveget iratjuk ki kozepen, kesobb van szerepe a jatek inditasanal es az esetleges kijatszas eseten.
 
 def draw_grid(surface, row, col):
-    sx = top_left_x
-    sy = top_left_y
+    sx = topleft_x
+    sy = topleft_y
     for i in range(row):
         pygame.draw.line(surface, (128, 128, 128), (sx, sy + i * 30),
-                         (sx + play_width, sy + i * 30))  # vizszintes
+                         (sx + p_width, sy + i * 30))  # vizszintes
         for j in range(col):
             pygame.draw.line(surface, (128, 128, 128), (sx + j * 30, sy),
-                             (sx + j * 30, sy + play_height))  # fuggoleges
+                             (sx + j * 30, sy + p_height))  # fuggoleges
+def clear_rows(grid, locked):
+    # vizsgaljuk, hogy az also sor folotti sor telivan-e hogy azoknak is megadhato legyen a torles.
 
+    inc = 0
+    for i in range(len(grid)-1,-1,-1):
+        row = grid[i]
+        if (0, 0, 0) not in row:
+            inc += 1
+            # acsusztatunk, poziciokat adunk be a foglalt helyeknek
+            ind = i
+            for j in range(len(row)):
+                try:
+                    del locked[(j, i)]
+                except:
+                    continue
+    if inc > 0:
+        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
+            x, y = key
+            if y < ind:
+                newKey = (x, y + inc)
+                locked[newKey] = locked.pop(key)
 
 main_menu()
